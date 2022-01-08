3243f03d833a58e6211921d744153f233fe5f374
misha <mixan_45@mail.ru>
2022-01-08 18:32:38 +0300

    v 5.1 full

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,3 +1,4 @@
+import *  as axios from 'axios';
 import React from 'react';
 import classes from './Users.module.css';
 import UsersItem from './Users_Item/UsersItem';
@@ -24,20 +25,42 @@ const Users = (props) => {
   //       </div>
   //     </div>
   // )
-  let usersElements = (dataU) => dataU.map (uElement => <UsersItem onFollow={props.onFollow} unFollow={props.unFollow} name={uElement.name} id={uElement.id} uFriend={uElement.friend} uStatus={uElement.status} uCity={uElement.location.city} uCountry={uElement.location.country} img_source={uElement.src} />)
+  // let usersElements = (dataU) => dataU.map (uElement => <UsersItem onFollow={props.onFollow} unFollow={props.unFollow} name={uElement.name} id={uElement.id} uFriend={uElement.friend} uStatus={uElement.status} uCity={uElement.location.city} uCountry={uElement.location.country} img_source={uElement.src} />)
+ 
+  if (props.users.length === 0) {
 
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => props.setUsers(response.data.items));
+}
+debugger;
 
   return (
-    <div className={classes.pole}>
-      <div>
-        <button>Show Users</button>
-      </div>
-      <div>
-        {usersElements(props.users)}
-      </div>
-    </div>
+    <div>
+      {
+        props.users.map(uElement =>
+          <div key={uElement.id}>
+            <span>
+              <div>
+                {/* <img src={uElement.src} /> */}
+              </div>
+              <div>
+                {uElement.followed ?
+                  <button onClick={() => props.unFollow(uElement.id)}>Unfollow</button> :
+                  <button onClick={() => props.onFollow(uElement.id)}>Follow</button>}
+              </div>
+            </span>
+
+            <span>
+              <div>{uElement.name}</div>
+            </span>
+            <span>
+              {/* <div>{uElement.location.city}, {uElement.location.country} , {uElement.status} </div> */}
+              <div>{uElement.status} </div>
+            </span>
+          </div>
+        )
+      }
+    </div >
   );
-  debugger
 }
 
 
--- src/redux/reduserUsers.js
+++ src/redux/reduserUsers.js
@@ -5,11 +5,11 @@ const SETUSERS = 'SETUSERS';
 
 let initialState = {
   users: [
-    { id: 1, name: "111", status: "Chill", friend: true, location: { city: "Minsk", country: "Belarus" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-3.png" },
-    { id: 2, name: "Vasya", status: "Sleep", friend: true, location: { city: "Moscow", country: "Russia" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-4.png" },
-    { id: 3, name: "Petya", status: "Playing", friend: false, location: { city: "New York", country: "USA" }, src: "https://img.icons8.com/external-linector-flat-linector/64/000000/external-avatar-man-avatar-linector-flat-linector-2.png" },
-    { id: 4, name: "Victor", status: "Full fun", friend: false, location: { city: "London", country: "GB" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-1.png" },
-    { id: 5, name: "Valera", status: "Free", friend: false, location: { city: "Rome", country: "Italy" }, src: "https://img.icons8.com/external-color-for-better-life-royyan-wijaya/64/000000/external-avatar-avatar-i-color-for-better-life-royyan-wijaya-16.png" }
+  //   { id: 1, name: "111", status: "Chill", friend: true, location: { city: "Minsk", country: "Belarus" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-3.png" },
+  //   { id: 2, name: "Vasya", status: "Sleep", friend: true, location: { city: "Moscow", country: "Russia" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-4.png" },
+  //   { id: 3, name: "Petya", status: "Playing", friend: false, location: { city: "New York", country: "USA" }, src: "https://img.icons8.com/external-linector-flat-linector/64/000000/external-avatar-man-avatar-linector-flat-linector-2.png" },
+  //   { id: 4, name: "Victor", status: "Full fun", friend: false, location: { city: "London", country: "GB" }, src: "https://img.icons8.com/external-linector-lineal-color-linector/64/000000/external-avatar-man-avatar-linector-lineal-color-linector-1.png" },
+  //   { id: 5, name: "Valera", status: "Free", friend: false, location: { city: "Rome", country: "Italy" }, src: "https://img.icons8.com/external-color-for-better-life-royyan-wijaya/64/000000/external-avatar-avatar-i-color-for-better-life-royyan-wijaya-16.png" }
   ]
 }
 
@@ -22,26 +22,22 @@ const reducerUsers = (state = initialState, action) => {
       };
     }
     case FOLLOW: {
-      // state.users[action.userId].friend = true;
-      // state.users[action.userId].status = "yyy"; 
       return {
         ...state,
         users: state.users.map(u => {
-          if (u.id = action.userId) {
-            return { ...u, friend: true }
+          if (u.id === action.userId) {
+            return { ...u, followed: true }
           }
           return u;
         })
       };
     }
     case UNFOLLOW: {
-      // state.users[action.userId].friend = false;
-      // state.users[action.userId].status = "nnnn";
       return {
         ...state,
         users: state.users.map(u => {
-          if (u.id = action.userId){
-            return { ...u, friend: false, status: "dsd" }
+          if (u.id === action.userId){
+            return { ...u, followed: false}
           }
           return u;
         })
@@ -52,7 +48,7 @@ const reducerUsers = (state = initialState, action) => {
       // state.users[action.userId].status = "nnnn";
       return {
         ...state,
-        users: [...state.users, ...action.users]
+        users: [...action.users]
     }
   }
     default:
