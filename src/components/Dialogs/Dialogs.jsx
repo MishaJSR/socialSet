dbc17fa8cd9d02288176b4bf04aed06ad4ff9b75
misha <mixan_45@mail.ru>
2022-03-13 21:42:02 +0300

    v 6.7

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,17 +4,6 @@ import { NavLink } from 'react-router-dom';
 import DialogItem from './DialogItem/DialogItem'
 import MessageItem from './Message/MessageItem'
 
-
-// let nameId = 3;
-
-let dialogsElements = (dataD) =>  dataD.map (dElement => <DialogItem name={dElement.name} id={dElement.id} />);
-
-let messageElements = (dataM) => dataM.map (mElement => <MessageItem message={mElement.content} />)
-
-// let messageUser = (dataM ,Id) => (<MessageItem message={dataM[nameId].content}/>);
-// let isActiveBut;
-// let dialogsElements = (dataD, isActiveBut) =>  dataD.map (dElement => <DialogItem active={isActiveBut} name={dElement.name} id={dElement.id} />);
-
 const Dialogs = (props) => {
 
     let textAreaText = React.createRef();
@@ -32,12 +21,10 @@ const Dialogs = (props) => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogs_items}>
-                {dialogsElements(props.state.dialogPage.dialogData)}
+                {props.state.dialogPage.dialogData.map (dElement => <DialogItem name={dElement.name} id={dElement.id} />)}
             </div>
             <div className={classes.messages}>
-                {/* {messageUser (props.messageData, nameId)} */}
-                {messageElements(props.state.dialogPage.messageData)}
-                
+                {props.state.dialogPage.messageData.map (mElement => <MessageItem message={mElement.content} />)}
                 <textarea name="" ref={textAreaText} onChange={onTextChange} cols="30" rows="10"></textarea>
                 <button onClick={addMessage}>Написать</button>
             </div>
